ce89b81009c3c116addd9d0439153fcc25fcbca9
syntat <AKShimalin@gmail.com>
2024-07-15 21:43:29 +0100

    camera adjusted to manual click scroll

--- Camera.gd
+++ Camera.gd
@@ -1,25 +1,36 @@
 extends Camera2D
 
-var resolution: Vector2
-var screen_centre: Vector2
 var velocity: Vector2
-# there is a circle in the centre of the screen of radius r that is used to check against mouse 
-# position, if it is outside the camera moves along the 'normal' with speed linearly proportional to the 
-# distance outside of the radius 
-var radius := 160
+var nail: Vector2
+var scrolling: bool
+
+# FIX: Problems with clamping of the camera - no smooth movement when skirting the edges
+
+var last_mouse_position: Vector2
+var current_mouse_position: Vector2
+var mouse_delta: Vector2
+var just_started_scroll: bool = true
 
 func _ready():
-	velocity = Vector2(0,0)
-	resolution = get_viewport_rect().size
-	position = resolution / 2
-	screen_centre = resolution / 2
-	print("RESOLUTION IS : ", resolution)
-func _process(delta):
-	var mouse_position: Vector2 = get_viewport().get_mouse_position()
-	var v: Vector2 = mouse_position - screen_centre
-	velocity = Vector2(0,0)
-	if v.length() > radius: 
-		velocity = (v) * 30 * delta 
+	scrolling = false
+	pass
 	
-	position += velocity * delta
+func _process(delta):
+	velocity = Vector2.ZERO
+	if scrolling:
+		current_mouse_position = get_local_mouse_position()
+		if just_started_scroll: 
+			last_mouse_position = current_mouse_position
+			just_started_scroll = false
+		mouse_delta = current_mouse_position - last_mouse_position 
+		last_mouse_position = current_mouse_position
+	if Input.is_action_pressed("scroll"):
+		scrolling = true
+	if Input.is_action_just_released("scroll"):
+		scrolling = false
+		just_started_scroll = true
+		mouse_delta = Vector2(0,0)
+	position -= mouse_delta
+	
+	
 	
